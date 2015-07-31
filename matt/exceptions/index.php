a85ef73df27adbf592c10a9df0d51484c5478df0
Gizmo <bundichmail@gmail.com>
2015-07-31 16:18:12 +0300

    Исключения

--- matt/exceptions/index.php
+++ matt/exceptions/index.php
@@ -7,10 +7,25 @@ class Conf {
 
     function __construct($file){
         $this->file = $file;
-        $this->xml = simplexml_load_file($file);
+
+        if(! file_exists($file)){
+            throw new FileException("Файл $file не существует");
+        }
+        $this->xml = simplexml_load_file($file, Null, LIBXML_NOERROR);
+        if(!is_object($this->xml)){
+            throw new XmlException(libxml_get_last_error());
+        }
+
+        $matches = $this->xml->xpath("/conf");
+        if(!count($matches)){
+            throw new ConfException("корневой элемент conf не найден");
+        }
     }
 
     function write(){
+        if(!is_writable($this->file)){
+            throw new FileException("файл $this->file не доступен для записи");
+        }
         file_put_contents($this->file, $this->xml->asXML());
     }
 
@@ -32,7 +47,45 @@ class Conf {
     }
 }
 
-$config = new Conf("file.xml");
-$config->set('sex','male'); // назначает значение
-$config->write(); // записывает в файл
-echo $config->get('sex'); // выводит значение
+Class XmlException extends Exception{ // класс оброботки XML-исключений, который дополняет основной класс исключений
+    private $error;
+
+    function __construct(LibXMLError $error){ // при создании исключения, принимает (ошибку) обьект класса LibXMLError
+        $shortfile = basename($error->file);
+        $msg = "[{$shortfile}, строка {$error->line}, ";
+        $msg .= "колонка {$error->column}] {$error->message}";
+        $this->error = $error;
+        parent::__construct($msg, $error->code); //передаёт в конструктор родительского класса сообщение, и код ошибки
+    }
+
+    function getLibXmlError(){
+        return $this->error;
+    }
+}
+
+class FileException extends Exception{}
+class ConfException extends Exception{}
+
+class Runner {
+    static function init(){
+        try{
+            $config = new Conf("file.xml");
+            $config->set('sex','male'); // назначает значение
+            $config->write(); // записывает в файл
+            echo "sex: ".$config->get('sex'); // выводит значение
+        }catch (FileException $e){
+            die($e->__toString()); //файл не существует либо не доступен для записи
+        }catch (XmlException $e){
+            die($e->__toString()); //Повреждённый XML-файл
+        }catch (ConfException $e){
+            die($e->__toString()); //не корректный формат XML-файла
+        }catch (Exception $e){
+            die($e->__toString()); // Этот код не долже никогда вызываться
+        }
+    }
+}
+
+Runner::init();
+
+
+
